d8f27faa9d2bd46a9d380197b4095c61e7b2513a
giuseppesciacca <sciaccagi@gmail.com>
2023-03-30 18:28:17 +0200

    [add] keyup.enter listner on input

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,11 @@ Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo vie
 
 MILESTONE 3
 Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
+Bonus:
+oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
+
+cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
  */
 const { createApp } = Vue
 
